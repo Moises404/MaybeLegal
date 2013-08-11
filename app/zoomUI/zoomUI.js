1a524db0dfdc2871e640d7b32094e6ad028d832a
Moises404 <sesmoi.moises@live.com>
2013-08-11 18:45:08 -0400

    added clicked events with css changes to the canvas-tool modes

--- app/zoomUI/zoomUI.js
+++ app/zoomUI/zoomUI.js
@@ -19,12 +19,58 @@ $( document ).ready(function() {
 	// APP STARTS //
 	////////////////
 	
-	var designCanvas = $('.canvas-ui');
-	var zoomButton = $('.canvas-zoom-button');
-	var zoomButtonMousedown = false;
-	var zoomButtonToggle_zoomIn = false;
-	var zoomButton_firstClick = true;
 
+
+	// initialize canvas-ui variables
+		var designCanvas = $('.canvas-ui');
+		var designButton = $('.canvas-design-button');
+		var productButton = $('.canvas-product-button');
+		var completeButton = $('.canvas-complete-button');
+		var zoomButton = $('.canvas-zoom-button');
+			
+			// checks logic for zoom animation
+				var zoomButtonMousedown = false;
+				var zoomButtonToggle_zoomIn = false;
+				var zoomButton_firstClick = true;
+
+
+	// canvas-ui button event handlers
+		designButton.click( function() {
+			console.log('design-mode');
+
+			// creates radio button logic
+				
+				//changes background & button icons
+				$(this).addClass('canvas-tool-button-active canvas-design-button-active');
+				$(this).removeClass('canvas-design-button');
+				productButton.removeClass('canvas-tool-button-active canvas-product-button-active');
+				productButton.addClass('canvas-product-button');
+				completeButton.removeClass('canvas-tool-button-active canvas-complete-button-active');
+				completeButton.addClass('canvas-complete-button');
+		});
+
+		productButton.click( function() {
+			console.log('product-mode');
+				
+				$(this).addClass('canvas-tool-button-active canvas-product-button-active');
+				$(this).removeClass('canvas-product-button');
+				designButton.removeClass('canvas-tool-button-active canvas-design-button-active');
+				designButton.addClass('canvas-design-button');
+				completeButton.removeClass('canvas-tool-button-active canvas-complete-button-active');
+				completeButton.addClass('canvas-complete-button');
+		});
+
+		completeButton.click( function() {
+			console.log('complete-mode');
+
+				$(this).addClass('canvas-tool-button-active canvas-complete-button-active');
+				$(this).removeClass('canvas-complete-button');
+				designButton.removeClass('canvas-tool-button-active canvas-design-button-active');
+				designButton.addClass('canvas-design-button');
+				productButton.removeClass('canvas-tool-button-active canvas-product-button-active');
+				productButton.addClass('canvas-product-button');
+
+		});
 
 	zoomButton.mousedown(function() {
 		console.log('zoom-button: mousedown');
