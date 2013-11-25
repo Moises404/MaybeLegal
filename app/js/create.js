507e03726bbfb1243362416a5f5bdcc8b499c3bf
Moises404 <sesmoi.moises@live.com>
2013-11-25 16:14:03 -0500

    another change

--- app/js/create.js
+++ app/js/create.js
@@ -912,8 +912,8 @@ $( document ).ready(function() {
 						var parsed_clicked_spriteImage = clicked_spriteImage.slice(100, clicked_spriteImage.length - 1);
 
 							console.log(clicked_spriteImage);
-							console.log(clicked_spriteImage.slice(100, 104));
-							console.log('dsd+++');	
+							console.log(clicked_spriteImage.slice(80, clicked_spriteImage.length - 1));
+							console.log('dsd+++dfg');	
 						
 						var design_canvas_dataPath = 'assets/imageBin/completed_designs/';
 						design_layer_1.css({'background-image': 'url("' + design_canvas_dataPath + parsed_clicked_spriteImage + '")'});
