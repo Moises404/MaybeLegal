2cbbb11a774a608b3959f582cb84976c4b992aba
Moises404 <sesmoi.moises@live.com>
2013-11-25 15:38:09 -0500

    another commit

--- app/js/create.js
+++ app/js/create.js
@@ -906,7 +906,8 @@ $( document ).ready(function() {
 					case 'design-mode':
 	
 						var clicked_spriteImage = $(this).css('background-image').toString();
-						var parsed_clicked_spriteImage = clicked_spriteImage.substr(125).substr(0, clicked_spriteImage.length-126);
+						//var parsed_clicked_spriteImage = clicked_spriteImage.substr(125).substr(0, clicked_spriteImage.length-126);
+						var parsed_clicked_spriteImage = clicked_spriteImage.substr(125).substr(10, clicked_spriteImage.length-126);
 
 							console.log(clicked_spriteImage);
 							console.log(parsed_clicked_spriteImage + 'dsd');
